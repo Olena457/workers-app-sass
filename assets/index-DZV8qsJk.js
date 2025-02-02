26bac8cd9daddf643e4106529f98875d07374afb
Olena457 <olenademianenko42@gmail.com>
2025-02-02 12:52:30 +0000

    Deploying to gh-pages from @ Olena457/workers-app-sass@5270e4a62d73f87f7eb4279d3db0f61ce72b5307 🚀

--- assets/index-DZV8qsJk.js
+++ assets/index-DZV8qsJk.js
@@ -35,3 +35,23 @@
     fetch(link.href, fetchOpts);
   }
 })();
+const refsMenu = {
+  openMenuBtn: document.querySelector(".js-menu-open"),
+  closeMenuBtn: document.querySelector(".js-menu-close"),
+  overlayMenu: document.querySelector(".js-menu")
+};
+const toggleMenu = () => {
+  const isMenuOpen = refsMenu.openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+  refsMenu.openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+  refsMenu.overlayMenu.classList.toggle("is-open");
+  constScrollLockMethod = !isMenuOpen ? "disableBodyScroll" : "enableBodyScroll";
+  bodyScrollLock[scrollLockMethod](document.body);
+};
+refsMenu.openMenuBtn.addEventListener("click", toggleMenu);
+refsMenu.closeMenuBtn.addEventListener("click", toggleMenu);
+window.matchMedia("(min-width: 1200px)").addEventListener("change", (e) => {
+  if (!e.matches) return;
+  refsMenu.overlayMenu.classList.remove("is-open");
+  refsMenu.openMenuBtn.setAttribute("aria-expanded", false);
+  bodyScrollLock.enableBodyScroll(document.body);
+});
