ffb0f0742b299ec7a78fd5a7b7573530c8f76843
Olena457 <olenademianenko42@gmail.com>
2025-02-03 00:02:07 +0000

    Deploying to gh-pages from @ Olena457/workers-app-sass@a6814405aab5dd31bd8fbccbd2d16ee76f09d61d 🚀

--- assets/index-D21AIPTg.js
+++ assets/index-D21AIPTg.js
@@ -44,14 +44,11 @@ const toggleMenu = () => {
   const isMenuOpen = refsMenu.openMenuBtn.getAttribute("aria-expanded") === "true" || false;
   refsMenu.openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
   refsMenu.overlayMenu.classList.toggle("is-open");
-  const scrollLockMethod = !isMenuOpen ? "disableBodyScroll" : "enableBodyScroll";
-  bodyScrollLock[scrollLockMethod](document.body);
 };
 refsMenu.openMenuBtn.addEventListener("click", toggleMenu);
 refsMenu.closeMenuBtn.addEventListener("click", toggleMenu);
-window.matchMedia("(min-width: 1200px)").addEventListener("change", (e) => {
-  if (!e.matches) return;
+window.matchMedia("(min-width: 1200px)").addEventListener("change", (event) => {
+  if (!event.matches) return;
   refsMenu.overlayMenu.classList.remove("is-open");
   refsMenu.openMenuBtn.setAttribute("aria-expanded", false);
-  bodyScrollLock.enableBodyScroll(document.body);
 });
